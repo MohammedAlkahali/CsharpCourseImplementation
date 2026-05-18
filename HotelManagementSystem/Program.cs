84f27414a7a58b2e5b3a802e70987cfa27f05be7
MohammedAlkahali <mohammedharib21@gmail.com>
2026-05-18 15:10:18 +0400

    Declared all the variables

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            // Declaring all the variables 
+            string guestName;
+            int guestPhone;
+            int roomNum;
+            int roomType;
+            int nightlyRate;
+            int checkInDate;
+            int checkOutDate;
+            int NumOfNights;
+            string roomNotes;
+            int disPercentage;
+            int loyaltyPoints;
+            bool isRegistered;
+            bool isCurrCheckedIn;
+
+            /////////////////////////////
+            
+
+
+        } // main
+    } // class program
+} // name space
