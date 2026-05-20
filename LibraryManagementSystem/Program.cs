052f87d5e493a82269ef01ba7f36ed404900cff8
MohammedAlkahali <mohammedharib21@gmail.com>
2026-05-20 09:38:54 +0400

    Declared all the variables

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        // Declare all the variables as a static above the main so functions share access.
+
+        static string memberName = "";
+        static int memberID = 0;
+        static string memberEmail = "";
+        static string membershipExpiryDate = "";
+        static int memberTier = 0;
+        static string bookTitle = "";
+        static string bookAuthor = "";
+        static string bookGenre = "";
+        static int numAvailableCopies = 0;
+        static bool isMemberRegistered = false;
+        static bool isBookRegistered = false;
+        static int totalBooksBorrowedThisSession = 0;
+        static int totalFinesPaidThisSession = 0;
+        //////////////////////////////////////////////////
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
