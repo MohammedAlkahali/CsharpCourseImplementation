7a19e9a837e540b9f823b800cc50a797c9910122
MohammedAlkahali <mohammedharib21@gmail.com>
2026-05-20 10:12:33 +0400

    Implement the main menu

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -22,7 +22,33 @@
 
         static void Main(string[] args)
         {
-            
-        }
-    }
-}
+            bool exit = false;
+            while (exit == false)
+
+            {           // Display the main menu for the user to choose
+
+                Console.WriteLine("Hello and welcome to library management system.");
+                Console.WriteLine("      Please select from the main menu");
+                Console.WriteLine();
+                Console.WriteLine("     1) Register a new member");
+                Console.WriteLine("     2) Display the member profile");
+                Console.WriteLine("     3) Search book by title");
+                Console.WriteLine("     4) Borrow a book");
+                Console.WriteLine("     5) Return a book");
+                Console.WriteLine("     6) Calculate late fine");
+                Console.WriteLine("     7) Applying discount for a member");
+                Console.WriteLine("     8) Check the borrowing eligibility");
+                Console.WriteLine("     9) Register a book");
+                Console.WriteLine("     10) Generate ID for a member");
+                Console.WriteLine("     11) Display book details");
+                Console.WriteLine("     12) Calculate renewal fee");
+                Console.WriteLine("     13) Update the email for a member");
+                Console.WriteLine("     14) The member details summary");
+                Console.WriteLine();
+                Console.Write("Select: ");
+                int Select = int.Parse(Console.ReadLine());
+
+            } // While loop
+        } // static main
+    } // class program
+} // namespace
