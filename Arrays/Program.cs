1c3111ce020cf5ab2b536333f7cf716ae9a8c9c3
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-01 16:35:01 +0400

    Implement the main menu

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,3 +8,30 @@
         }
     }
 }
+
+
+
+
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (exit == false)
+
+            {
+                Console.WriteLine("  Please select from the main menu");
+                Console.WriteLine();
+                Console.WriteLine("   1) Temperature Log ");
+                Console.WriteLine("   2) Student Score Board");
+                Console.WriteLine("   3) Product Price Finder  ");
+                Console.WriteLine("   4) Race Finish Times ");
+                Console.WriteLine("   5) Classroom Grade Report ");
+                Console.WriteLine("   6) Warehouse Inventory Check");
+                Console.WriteLine("   7) Library Book Shelf Scanner");
+                Console.WriteLine("   8) Sales Performance Analyzer");
+                Console.WriteLine("   9) Flight Seat Allocation Displayk");
+                Console.WriteLine("   10) Hospital Patient Priority Queue ");
+                Console.WriteLine("   0) Exit");
+
+                Console.Write("Select: ");
+
+                int Select = int.Parse(Console.ReadLine());
