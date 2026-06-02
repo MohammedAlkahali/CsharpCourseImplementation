4d75cb9e926e644f44a2b124e7213df7466a9c02
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-02 10:54:06 +0400

    Complete Problem 3: Product Price Finder

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -13,7 +13,7 @@
                 {
                     Console.Write("The temperature for day " + (i + 1) + " is: ");
                     Console.WriteLine(temperatures[(int)i]);
-                    
+
                 }
 
             }
@@ -27,81 +27,97 @@
         {
             int[] scores = { 54, 86, 44, 76, 87, 61 };
 
-            foreach (int i in scores) 
+            foreach (int i in scores)
             {
-                
+
                 {
-                    Console.Write("The score of the student " );
+                    Console.Write("The score of the student ");
                     Console.WriteLine(i);
-                    
+
                 }
 
                 Array.Reverse(scores);
                 Console.WriteLine(scores);
 
             }
-                
+
         }
-           
-           
 
 
 
-        static void Main(string[] args)
+
+        public static void price()
         {
-            
-            bool exit = false;
-            while (exit == false)
+            double[] prices = { 7.5, 6, 3.8, 9, 4 };
 
+            for (int i = 0 ; i < prices.Length ; i++)
             {
-                Console.WriteLine("  Please select from the main menu");
-                Console.WriteLine();
-                Console.WriteLine("   1) Temperature Log ");
-                Console.WriteLine("   2) Student Score Board");
-                Console.WriteLine("   3) Product Price Finder  ");
-                Console.WriteLine("   4) Race Finish Times ");
-                Console.WriteLine("   5) Classroom Grade Report ");
-                Console.WriteLine("   6) Warehouse Inventory Check");
-                Console.WriteLine("   7) Library Book Shelf Scanner");
-                Console.WriteLine("   8) Sales Performance Analyzer");
-                Console.WriteLine("   9) Flight Seat Allocation Displayk");
-                Console.WriteLine("   10) Hospital Patient Priority Queue ");
-                Console.WriteLine("   0) Exit");
+                Console.Write("Price of the product " + (i + 1) + " is: ");
+                Console.WriteLine(prices[i] + " OMR");
 
-                Console.Write("Select: ");
+            }
 
-                int Select = int.Parse(Console.ReadLine());
 
-                switch (Select)
+            int index = Array.IndexOf(prices, 9);
+            Console.WriteLine("The index of the number 9 is " + index);
+        }
+            static void Main(string[] args)
+            {
+
+                bool exit = false;
+                while (exit == false)
+
                 {
-                    case 0:
-                        exit = true;
-                        Console.WriteLine("Goodbye!");
-                        break;
+                    Console.WriteLine("  Please select from the main menu");
+                    Console.WriteLine();
+                    Console.WriteLine("   1) Temperature Log ");
+                    Console.WriteLine("   2) Student Score Board");
+                    Console.WriteLine("   3) Product Price Finder  ");
+                    Console.WriteLine("   4) Race Finish Times ");
+                    Console.WriteLine("   5) Classroom Grade Report ");
+                    Console.WriteLine("   6) Warehouse Inventory Check");
+                    Console.WriteLine("   7) Library Book Shelf Scanner");
+                    Console.WriteLine("   8) Sales Performance Analyzer");
+                    Console.WriteLine("   9) Flight Seat Allocation Displayk");
+                    Console.WriteLine("   10) Hospital Patient Priority Queue ");
+                    Console.WriteLine("   0) Exit");
 
-                    case 1:
-                        Temp();
-                        break;
-                    case 2:
-                        Score();
-                        
-                        break;
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                    case 11:
+                    Console.Write("Select: ");
 
-                    default:
-                        Console.WriteLine("Invalid option. Please select a valid number.");
-                        break;
+                    int Select = int.Parse(Console.ReadLine());
 
-                } // Switch
-            } // While
-        } // Main
-    } // Program
-} // Namespace
+                    switch (Select)
+                    {
+                        case 0:
+                            exit = true;
+                            Console.WriteLine("Goodbye!");
+                            break;
+
+                        case 1:
+                            Temp();
+                            break;
+                        case 2:
+                            Score();
+
+                            break;
+                        case 3:
+                            price();
+                            break;
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+                        case 11:
+
+                        default:
+                            Console.WriteLine("Invalid option. Please select a valid number.");
+                            break;
+
+                    } // Switch
+                } // While
+            } // Main
+        } // Program
+    } // Namespace
