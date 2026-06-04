56fbcec6c0bc06a977f412fa9092c23bb6f87eeb
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-04 15:37:53 +0400

    Implement the main menu for the Stack & Queue Practice

--- Stack&Queue/Program.cs
+++ Stack&Queue/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exit = false;
+            while (exit == false)
+
+            {
+                Console.WriteLine("  Please select from the main menu");
+                Console.WriteLine();
+                Console.WriteLine("   1) Browser History Tracker                     ");
+                Console.WriteLine("   2) Hotel Check-In Queue                        ");
+                Console.WriteLine("   3) Text Editor Undo System                     ");
+                Console.WriteLine("   4) Hospital Emergency Room Triage              ");
+                Console.WriteLine("   5) Parenthesis Validator                       ");
+                Console.WriteLine("   6) Print Spooler with Priority Re-Insertion    ");
+                Console.WriteLine("   7) Reverse a Sentence Word by Word             ");
+                Console.WriteLine("   8) Multi-Level Undo with Redo                  ");
+                Console.WriteLine("   9) Ticket Counter Simulation                   ");
+                Console.WriteLine("   10) Order Processing Pipeline with Statistics  ");
+                Console.WriteLine("   0) Exit                                        ");
+
+                Console.Write("Select: ");
+
+                int Select = int.Parse(Console.ReadLine());
         }
     }
 }
